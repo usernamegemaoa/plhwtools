721d49cd85683891f551942e143a73fb9ee3405e
Guillaume Tucker <guillaume.tucker@plasticlogic.com>
2013-06-17 16:46:21 +0100

    Android.mk: list all source files to fix Android NDK build

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := plhwtools
 LOCAL_MODULE_TAGS := eng
 LOCAL_CFLAGS += -Wall -O2
-LOCAL_SRC_FILES := $(call all-subdir-c-files)
+LOCAL_SRC_FILES := plhwtools.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libplhw
 LOCAL_STATIC_LIBRARIES := libplhw
 include $(BUILD_EXECUTABLE)
